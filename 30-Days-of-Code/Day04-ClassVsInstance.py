7ac563dde09e7c24329ca60f85861f0881fcaf92
SADIK KUZU <sadikkuzu@aof.anadolu.edu.tr>
2018-08-14 10:47:20 +0300

    Day 4: Class vs. Instance

---
+++ 30-Days-of-Code/Day04-ClassVsInstance.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        self.age = initialAge
+        if self.age < 0:
+            print "Age is not valid, setting age to 0."
+            self.age = 0
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print "You are young."
+        elif self.age >= 18:
+            print "You are old."
+        else:
+            print "You are a teenager."
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age += 1
