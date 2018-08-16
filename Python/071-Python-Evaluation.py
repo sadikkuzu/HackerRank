ac814860eb6fce04ea3ffdc5fabba9ff0c218dd5
SADIK KUZU <sadikkuzu@aof.anadolu.edu.tr>
2018-08-17 00:18:44 +0300

    Python eval

---
+++ Python/071-Python-Evaluation.py
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/python-eval
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import print_function
+eval(raw_input())
